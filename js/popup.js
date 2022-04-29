091551b1f58e30c6a9be62186c677ed2289699ca
JanMaki <mutsukimaki@gmail.com>
2022-04-29 10:39:54 +0900

    更新 IDの入力欄部分を作成
    IDの入力欄を作成
    開き直しても、保存されるように

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,27 @@
-const log = function(){
+//IDの変数 保存されているIDから取得
+let id = localStorage.getItem("ID")
+
+//IDの入力欄を取得
+const idInputElement = document.getElementById("id_input")
+
+//入力欄の値にIDを代入
+idInputElement.value = id
+
+//定期的に処理をするタスク
+const task = function(){
+    //入力欄の値を取得
+    const input = idInputElement.value
+    //入力欄が更新されているかを確認
+    if (input !== id){
+        //IDを変更
+        id = input
+        //IDを保存
+        localStorage.setItem("ID", id)
+    }
+
+    //TODO 以下タイマーの更新処理を行う
     document.getElementById('timer-1').innerHTML = ""
 };
 
-setInterval(log, 1000);
+//タスクを開始
+setInterval(task, 500);
